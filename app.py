38b22f97666fe32708823da7a0457ebe21e70621
buts00 <butsprog@gmail.com>
2023-04-16 19:12:00 +0300

    Final

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from tkinter import *
 from tkinter import ttk
 import expenses_helper as eh
 from tkcalendar import DateEntry
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.font_manager as font_manager
+import seaborn as sns
 
 
 class App(Tk):
@@ -10,25 +14,55 @@ class App(Tk):
     def __init__(self):
         super().__init__()
         self.title('Expenses')
+        self.resizable(0, 0)
         self['background'] = '#EBEBEB'
-        self.resizable(0,0)
         self.iconbitmap('icon.ico')
         self.style = ttk.Style()
         self.style.theme_use('xpnative')
         self.conf = {'padx': {10, 10}, 'pady': 10}
         self.bold_font = 'Helvetica 11 bold'
+        self.put_btn_graph()
         self.put_frames()
 
     def put_frames(self):
-        self.frame_add_form = AddForm(self).grid(column=0, row=0, sticky='ns')
-        self.self = StatFrame(self).grid(column=1, row=0, sticky='ns')
-        self.frame_list = ListFrame(self).grid(column=0, row=1, columnspan=2, sticky='we')
+        self.frame_graph = Graph(self)
+        self.frame_list = ListFrame(self)
+        self.frame_add_form = AddForm(self)
+        self.statistic = StatFrame(self)
+        self.frame_pie_graph = PieGraph(self)
+        self.frame_add_form.grid(column=0, row=0, sticky='ns')
+        self.check_btn()
+
+    def put_btn_graph(self):
+        self.enabled = IntVar()
+        self.checkbutton = ttk.Checkbutton(text="Graph", variable=self.enabled,
+                                           command=lambda: self.check_btn())
+        self.checkbutton.lift()
+        self.checkbutton.configure(takefocus=0)
+        self.checkbutton.place(relx=0.5, rely=0.375, anchor='center')
 
     def refresh(self):
         all_f = [f for f in self.children]
         for frame in all_f:
-            self.nametowidget(frame).destroy()
+            if self.nametowidget(frame) != self.nametowidget(self.checkbutton):
+                self.nametowidget(frame).destroy()
         self.put_frames()
+
+    def check_btn(self):
+        if self.enabled.get():
+            self.checkbutton.lift()
+            self.checkbutton.place(relx=0.52, rely=0.32, anchor='center')
+            self.statistic.grid_forget()
+            self.frame_pie_graph.grid(column=1, row=0, sticky='ns')
+            self.frame_list.grid_forget()
+            self.frame_graph.grid(column=0, row=1, columnspan=2, sticky='we')
+        else:
+            self.checkbutton.lift()
+            self.frame_pie_graph.grid_forget()
+            self.statistic.grid(column=1, row=0, sticky='ns')
+            self.checkbutton.place(relx=0.55, rely=0.375, anchor='center')
+            self.frame_graph.grid_forget()
+            self.frame_list.grid(column=0, row=1, columnspan=2, sticky='we')
 
 
 class AddForm(Frame):
@@ -38,15 +72,16 @@ class AddForm(Frame):
         self.put_widgets()
 
     def put_widgets(self):
-        self.get_all_names_of_payments()
+        self.all_expenses = eh.get_all_expenses_name()
         self.l_choose = ttk.Label(self, text='Choose an expense item')
-        self.f_choose = ttk.Combobox(self, values=self.items)
+        self.f_choose = ttk.Combobox(self, values=self.all_expenses)
         self.l_amount = ttk.Label(self, text='Enter amount')
-        self.f_amount = ttk.Entry(self, validate='key', validatecommand=(self.register(self.validate_amount),'%P'))
+        self.f_amount = ttk.Entry(self, validate='key', validatecommand=(self.register(self.validate_amount), '%P'))
         self.l_date = ttk.Label(self, text='Enter date')
         self.f_date = DateEntry(self, foreground='black', normalforeground='black', selectforeground='red',
                                 selectbackground='white', background='white', date_pattern='mm-dd-YYYY')
-        self.btn_submit = ttk.Button(self, text='submit', command=self.add_form)
+        self.btn_submit = ttk.Button(self, text='Submit', command=self.add_form)
+        self.btn_del = ttk.Button(self, text='Delete', command=lambda: self.master.frame_list.delete_form())
 
         self.l_choose.grid(row=0, column=0, sticky='w', cnf=self.master.conf)
         self.f_choose.grid(row=0, column=1, sticky='e', cnf=self.master.conf)
@@ -54,7 +89,8 @@ class AddForm(Frame):
         self.f_amount.grid(row=1, column=1, sticky='e', cnf=self.master.conf)
         self.l_date.grid(row=2, column=0, sticky='w', cnf=self.master.conf)
         self.f_date.grid(row=2, column=1, sticky='e', cnf=self.master.conf)
-        self.btn_submit.grid(row=3, column=0, columnspan=2, cnf=self.master.conf)
+        self.btn_submit.grid(row=3, column=1,sticky='e', cnf=self.master.conf)
+        self.btn_del.grid(row=3, column=0,sticky='w', cnf=self.master.conf)
 
     def validate_amount(self, input):
         if input == '':
@@ -66,12 +102,6 @@ class AddForm(Frame):
             return False
             self.bell()
 
-    def get_all_names_of_payments(self):
-        all_expenses = eh.get_all_expenses()
-        self.items = []
-        for expense in all_expenses:
-            self.items.append(expense.name)
-
     def add_form(self):
         flag = True
         name = str(self.f_choose.get())
@@ -79,19 +109,16 @@ class AddForm(Frame):
         date = datetime.strptime(self.f_date.get(), '%m-%d-%Y').date()
         self.l_choose['foreground'] = 'black'
         self.l_amount['foreground'] = 'black'
-
         if name == '':
             self.l_choose['foreground'] = 'red'
             self.bell()
             flag = False
-
         try:
-           amount = float(self.f_amount.get())
+            amount = float(self.f_amount.get())
         except ValueError:
             self.l_amount['foreground'] = 'red'
             flag = False
             self.bell()
-
         if flag:
             eh.create_new_form(name, amount, date)
             self.master.refresh()
@@ -127,25 +154,77 @@ class ListFrame(Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self['background'] = self.master['background']
-        self.put_widgets()
+        self.put_widgets_table()
 
-    def put_widgets(self):
+    def put_widgets_table(self):
         list_of_payments = eh.get_all_payments()
-        table = ttk.Treeview(self, show='headings')
-        heads = ['id', 'name', 'amount', 'date']
-        table['column'] = heads
-        for header in heads:
-            table.heading(header, text=header, anchor='center')
-            table.column(header, anchor='center')
+        self.table = ttk.Treeview(self, show='headings')
+        self.heads = ['id', 'name', 'amount', 'date']
+        self.table['column'] = self.heads
+        for header in self.heads:
+            self.table.heading(header, text=header, anchor='center')
+            self.table.column(header, anchor='center')
 
         for row in list_of_payments:
             value = (row.id, eh.Expense[row.payment_id].name, row.amount, row.payment_date)
-            table.insert('', END, values=value)
+            self.table.insert('', END, values=value)
+        self.table.bind("<Delete>", lambda event: self.delete_form())
+        self.scr = ttk.Scrollbar(self, command=self.table.yview)
+        self.table.configure(yscrollcommand=self.scr.set)
+        self.scr.pack(side=RIGHT, fill=Y)
+        self.table.pack(expand=YES, fill=BOTH)
 
-        scr = ttk.Scrollbar(self, command=table.yview)
-        table.configure(yscrollcommand=scr.set)
-        scr.pack(side=RIGHT, fill=Y)
-        table.pack(expand=YES, fill=BOTH)
+    def delete_form(self):
+        try:
+            self.cur_item = self.table.focus()
+            self.item_id = self.table.item(self.cur_item)['values'][0]
+            eh.delete_form(self.item_id)
+            self.table.delete(self.cur_item)
+            self.master.refresh()
+        except IndexError:
+            pass
+
+
+class Graph(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.put_widgets()
+
+    def put_widgets(self):
+        self.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        self.amount = eh.get_amount_for_month()
+        self.fig = plt.Figure(figsize=(8, 3), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self.ax.bar(self.months, self.amount,
+                    color=["#4c00ff", "#19278c", '#4df065', '#24d63f', "#0aad23", "#f3b197", "#f7926a", '#ed7140',
+                           '#ff9c38',
+                           '#de7b16', '#c98540', "#00aeff"], edgecolor="gray", linewidth=1)
+        self.ax.set_title("Statistic", fontsize=16)
+        self.ax.grid(axis='y', linestyle='--', linewidth=0.5)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.fig.subplots_adjust(right=0.95,left= 0.1)
+        self.canvas.draw()
+        self.fig.set_facecolor(self.master['background'])
+        self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+
+
+class PieGraph(Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.put_widgets()
+
+    def put_widgets(self):
+        self.all_name,self.all_amount = eh.get_all_expenses_dict()
+        self.fig = plt.Figure(figsize=(3.7, 1.5), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self.ax.pie(self.all_amount, autopct='%1.1f%%', startangle=0, shadow=True,rotatelabels=False,pctdistance=0.75, textprops={ 'size': 7},colors = sns.color_palette('pastel'))
+        self.ax.legend(title='Category', labels=self.all_name, loc='upper left', bbox_to_anchor=(-0.4, 1.1),prop=font_manager.FontProperties(size=8))
+        self.ax.axis('equal')
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.fig.subplots_adjust(right=1.1,left=0.45)
+        self.canvas.draw()
+        self.fig.set_facecolor(self.master['background'])
+        self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 
 
 app = App()
--- expenses_helper.py
+++ expenses_helper.py
@@ -1,15 +1,16 @@
 from models import *
 
 
-def get_all_expenses(db=db):
-    with db as db:
-        all_expenses = Expense.select()
-    return all_expenses
+def get_all_expenses_name():
+    all_expenses = Expense.select()
+    all = []
+    for el in all_expenses:
+        all.append(el.name)
+    return all
 
 
-def get_all_payments(db=db):
-    with db as db:
-        all_payments = Payment.select().join(Expense)
+def get_all_payments():
+    all_payments = Payment.select().join(Expense)
     return all_payments
 
 
@@ -62,8 +63,46 @@ def create_new_form(name, amount, date):
     p_id = Expense.select().where(Expense.name == name)
     try:
         p_id = p_id[0]
-    except:
+    except IndexError:
         Expense(name=name).save()
         all_id = Expense.select()
         p_id = all_id[len(all_id) - 1]
     Payment(amount=amount, payment_date=date, payment_id=p_id).save()
+
+
+def delete_form(id_item):
+    s = Payment.select().where(Payment.id == id_item)
+    expenses_id = s[0].payment_id
+    if len(Payment.select().where(Payment.payment_id == expenses_id)) == 1:
+        expenses_to_del = Expense.get(Expense.id == expenses_id)
+        expenses_to_del.delete_instance()
+    obj = Payment.get(Payment.id == id_item)
+    obj.delete_instance()
+
+
+def get_amount_for_month():
+    amount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    all_payments = get_all_payments()
+    for p in all_payments:
+        date = p.payment_date
+        date = date.month - 1
+        amount[date] += p.amount
+    return amount
+
+
+def get_all_expenses_dict():
+    all_expenses = {}
+    all_payments = get_all_payments()
+    for el in all_payments:
+        if Expense[el.payment_id].name in all_expenses:
+            all_expenses[Expense[el.payment_id].name] += el.amount
+        else:
+            all_expenses[Expense[el.payment_id].name] = el.amount
+    all_amount = list(all_expenses.values())
+    all_name = list(all_expenses.keys())
+    for i in range(0,len(all_name)):
+        all_name[i] += ' ('+str(int(all_amount[i]))+')'
+    return all_name,all_amount
+
+
+
